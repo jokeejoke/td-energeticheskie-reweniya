3953883f54eb45d5ed72401f00b1d72f3631bd2d
jokeejoke <69756862+jokeejoke@users.noreply.github.com>
2021-01-28 21:58:28 +0600

    add header

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	// Custom JS
+	const phoneMask = IMask(
+		document.querySelector('.phone'), {
+			mask: '+{7}(000)000-00-00'
+		});
+
+	function dropdownHover() {
+		$hasChildLink = document.querySelectorAll('.navbar__item-child')
+		$hasChildLink.forEach(el => {
+			el.addEventListener('mouseover', function() {
+				this.childNodes[0].nextElementSibling.style.color = '#ffa25c'
+				this.childNodes[0].nextElementSibling.children[0].style.transform = 'rotate(180deg)'
+				this.childNodes[0].nextElementSibling.children[0].style.fill = '#ffa25c'
+			})
+		});
+		$hasChildLink.forEach(el => {
+			el.addEventListener('mouseout', function() {
+				this.childNodes[0].nextElementSibling.style.color = '#ffffff'
+				this.childNodes[0].nextElementSibling.children[0].style.transform = 'rotate(0)'
+				this.childNodes[0].nextElementSibling.children[0].style.fill = '#ffffff'
+			})
+		});
+	}
+
+	dropdownHover()
 
 });
