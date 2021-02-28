33f9573164c9881d8aa569452ff6f78bfb4d333b
jokeejoke <69756862+jokeejoke@users.noreply.github.com>
2021-02-28 12:22:37 +0600

    change proizvod page full

--- app/js/app.js
+++ app/js/app.js
@@ -384,6 +384,27 @@ document.addEventListener("DOMContentLoaded", function() {
 		})
 	}
 
+	function accordion() {
+		const $acc = document.querySelectorAll('.accordion__header')
+		$acc.forEach(item => {
+			item.addEventListener('click', function () {
+				const $currentlyActuveAccHeader = document.querySelector('.accordion__header.accordion__header-active')
+				if ($currentlyActuveAccHeader && $currentlyActuveAccHeader !== item) {
+					$currentlyActuveAccHeader.classList.toggle('accordion__header-active')
+					$currentlyActuveAccHeader.nextElementSibling.style.maxHeight = 0
+				}
+
+				this.classList.toggle('accordion__header-active')
+				const $accBody = this.nextElementSibling
+				if (this.classList.contains('accordion__header-active')) {
+					$accBody.style.maxHeight = $accBody.scrollHeight + 'px'
+				} else {
+					$accBody.style.maxHeight = 0
+				}
+			})
+		})
+	}
+
 
 	function dropdownMenu () {
 		const $dropdownItem = document.querySelectorAll('.navbar__item-child')
@@ -629,6 +650,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	projectsSectionAnimation()
 	newsSectionAnimation()
 	contactsSectionAnimation()
+	accordion()
 	tablePadding()
 	mapHover()
 	initScrollBar('.certificates-gallery', '.certificates-gallery__progress')
